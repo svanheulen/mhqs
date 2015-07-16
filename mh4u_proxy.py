83c96254443d58d5ac3d880cd7a26dc88e3dc40c
Seth VanHeulen <svanheulen@gmail.com>
2015-07-16 08:38:35 -0400

    Revert "load mib files in path"
    This reverts commit 1012e45f4638205c53e7d3c5df9cc44d7c9b548d.

--- mh4u_proxy.py
+++ mh4u_proxy.py
@@ -16,56 +16,49 @@ import mhef.n3ds
 import proxy
 
 
-def make_quests(path, cipher, language, quest_path):
+def make_quests(path, cipher, language, quest_files):
     quests_page = ''
-    i = 0
-    for quest_file in os.listdir(quest_path):
-        quest_fullpath = os.path.join(quest_path, quest_file)
-        if os.path.isfile(quest_fullpath) and quest_file[-4:] == '.mib':
-            quest = open(quest_fullpath, 'rb')
-            quest.seek(0xa0)
-            info = struct.unpack('8I2H3B33x5H', quest.read(82))
-            quest.seek(info[7])
-            language_offset = struct.unpack('5I', quest.read(20))
-            lang_id = 0
-            if language == 'fre':
-                lang_id = 1
-            elif language == 'spa':
-                lang_id = 2
-            elif language == 'ger':
-                lang_id = 3
-            elif language == 'ita':
-                lang_id = 4
-            quest.seek(language_offset[lang_id])
-            text_offset = struct.unpack('7I', quest.read(28))
-            quest.seek(text_offset[0])
-            title = quest.read(text_offset[1] - text_offset[0]).decode('utf-16').strip('\x00')
-            success = quest.read(text_offset[2] - text_offset[1]).decode('utf-16').strip('\x00').split('\n')
-            if len(success) < 2:
-                success.append(' ')
-            success = '|'.join(success)
-            failure = quest.read(text_offset[3] - text_offset[2]).decode('utf-16').strip('\x00').split('\n')
-            if len(failure) < 2:
-                failure.append(' ')
-            failure = '|'.join(failure)
-            summary = quest.read(text_offset[4] - text_offset[3]).decode('utf-16').strip('\x00').split('\n')
-            if len(summary) < 7:
-                summary.extend([' '] * (7 - len(summary)))
-            summary = '|'.join(summary)
-            main_monsters = quest.read(text_offset[5] - text_offset[4]).decode('utf-16').strip('\x00').split('\n')
-            if len(main_monsters) < 2:
-                main_monsters.append(' ')
-            main_monsters = '|'.join(main_monsters)
-            client = quest.read(text_offset[6] - text_offset[5]).decode('utf-16').strip('\x00')
-            sub_quest = quest.read(language_offset[lang_id] - text_offset[6]).decode('utf-16').strip('\x00')
-            quests_page += time.strftime('%Y%m%d') + u'{:02d}|{:05d}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}\n'.format(i, info[8], title, info[0], info[9], 0, info[10], info[5], info[2], info[1], info[13], info[14], info[15], info[16], info[17], info[11], info[12], success, sub_quest, failure, main_monsters, client, summary)
-            quest.seek(0)
-            open(os.path.join(path, 'm{:05d}.mib'.format(info[8])), 'wb').write(cipher.encrypt(quest.read()))
-            quest.close()
-            i += 1
-            print '[+] Load Quest File : ' + title
-    
-        
+    for i in range(len(quest_files)):
+        quest = open(quest_files[i], 'rb')
+        quest.seek(0xa0)
+        info = struct.unpack('8I2H3B33x5H', quest.read(82))
+        quest.seek(info[7])
+        language_offset = struct.unpack('5I', quest.read(20))
+        lang_id = 0
+        if language == 'fre':
+            lang_id = 1
+        elif language == 'spa':
+            lang_id = 2
+        elif language == 'ger':
+            lang_id = 3
+        elif language == 'ita':
+            lang_id = 4
+        quest.seek(language_offset[lang_id])
+        text_offset = struct.unpack('7I', quest.read(28))
+        quest.seek(text_offset[0])
+        title = quest.read(text_offset[1] - text_offset[0]).decode('utf-16').strip('\x00')
+        success = quest.read(text_offset[2] - text_offset[1]).decode('utf-16').strip('\x00').split('\n')
+        if len(success) < 2:
+            success.append(' ')
+        success = '|'.join(success)
+        failure = quest.read(text_offset[3] - text_offset[2]).decode('utf-16').strip('\x00').split('\n')
+        if len(failure) < 2:
+            failure.append(' ')
+        failure = '|'.join(failure)
+        summary = quest.read(text_offset[4] - text_offset[3]).decode('utf-16').strip('\x00').split('\n')
+        if len(summary) < 7:
+            summary.extend([' '] * (7 - len(summary)))
+        summary = '|'.join(summary)
+        main_monsters = quest.read(text_offset[5] - text_offset[4]).decode('utf-16').strip('\x00').split('\n')
+        if len(main_monsters) < 2:
+            main_monsters.append(' ')
+        main_monsters = '|'.join(main_monsters)
+        client = quest.read(text_offset[6] - text_offset[5]).decode('utf-16').strip('\x00')
+        sub_quest = quest.read(language_offset[lang_id] - text_offset[6]).decode('utf-16').strip('\x00')
+        quests_page += time.strftime('%Y%m%d') + u'{:02d}|{:05d}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}\n'.format(i, info[8], title, info[0], info[9], 0, info[10], info[5], info[2], info[1], info[13], info[14], info[15], info[16], info[17], info[11], info[12], success, sub_quest, failure, main_monsters, client, summary)
+        quest.seek(0)
+        open(os.path.join(path, 'm{:05d}.mib'.format(info[8])), 'wb').write(cipher.encrypt(quest.read()))
+        quest.close()
     return cipher.encrypt(b'\xef\xbb\xbf' + quests_page.encode('utf-8'))
 
 
@@ -111,8 +104,8 @@ def make_root(root, region, language, event, challenge):
 parser = argparse.ArgumentParser(description='Runs a proxy for serving custom MH4U DLC quests.')
 parser.add_argument('region', choices=('JPN', 'USA', 'EUR', 'KOR'), help='your game region')
 parser.add_argument('language', choices=('jpn', 'eng', 'fre', 'spa', 'ger', 'ita', 'kor'), help='your game language')
-parser.add_argument('--event', help='the decrypted event quest files to serve')
-parser.add_argument('--challenge', help='the decrypted challenge quest files to serve')
+parser.add_argument('--event', nargs='+', help='the decrypted event quest files to serve')
+parser.add_argument('--challenge', nargs='+', help='the decrypted challenge quest files to serve')
 args = parser.parse_args()
 
 root = tempfile.mkdtemp()
